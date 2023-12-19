6eeef93015e86e5c5d4a318a289a25a1479ef197
mzoesch <magnus.zoeschinger@zoeschinger.de>
2023-12-19 22:57:23 +0100

    Update Voxel.h to spaces

--- unreal/Source/JAFG/Public/World/Voxel.h
+++ unreal/Source/JAFG/Public/World/Voxel.h
@@ -3,13 +3,13 @@
 UENUM()
 enum EVoxel
 {
-	Null,
-	Air,
-	Stone,
-	Dirt,
-	Grass,
-	Glass,
-	Log,
-	Planks,
-	Leaves,
+    Null,
+    Air,
+    Stone,
+    Dirt,
+    Grass,
+    Glass,
+    Log,
+    Planks,
+    Leaves,
 };
